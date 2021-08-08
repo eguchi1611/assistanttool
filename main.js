daa232db95867090658cb85d74ee6c34b2e141a3
Keita Ito <hlfcrstl@gmail.com>
2021-08-08 12:12:36 +0900

    コントロールパネルの作成

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 function doGet(e) {
-  return ContentService.createTextOutput("TEST");
+  return HtmlService.createTemplateFromFile("control").evaluate().addMetaTag("viewport", "width=device-width, initial-scale=1.0").setTitle("コントロールパネル - 健康観察フォーム入力支援ツール");
+}
+
+function register(target, json) {
+  try {
+    const KEY_FORMS = "forms";
+    let properties = PropertiesService.getScriptProperties();
+    let current = JSON.parse(properties.getProperty(KEY_FORMS)) || {};
+    current[target] = json;
+    properties.setProperty(KEY_FORMS, JSON.stringify(current));
+  } catch (error) {
+    return error.message;
+  }
+  return 0;
 }
