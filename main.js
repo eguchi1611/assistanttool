443a98dc49dc28a64d847c0f61a3f06039f44b1e
Keita Ito <hlfcrstl@gmail.com>
2021-08-09 15:42:08 +0900

    コントロールパネルに削除を追加

--- main.js
+++ main.js
@@ -15,7 +15,30 @@ function register(target, json) {
   return 0;
 }
 
+function deleteForm(target) {
+  try {
+    const KEY_FORMS = "forms";
+    let properties = PropertiesService.getScriptProperties();
+    let current = JSON.parse(properties.getProperty(KEY_FORMS)) || {};
+    delete current[target];
+    properties.setProperty(KEY_FORMS, JSON.stringify(current));
+  } catch (error) {
+    return error.message;
+  }
+  return 0;
+}
+
 function download() {
   const KEY_FORMS = "forms";
-  return JSON.parse(PropertiesService.getScriptProperties().getProperty(KEY_FORMS)) || {};
+  let temp = {};
+  for (let i = 0; i < 4; i++) {
+    temp[new Date().getFullYear() - i] = [];
+  }
+
+  let data = JSON.parse(PropertiesService.getScriptProperties().getProperty(KEY_FORMS)) || {};
+  for (let key of Object.keys(data)) {
+    temp[key] = JSON.parse(data[key]);
+  }
+  console.log(JSON.stringify(temp));
+  return temp;
 }
